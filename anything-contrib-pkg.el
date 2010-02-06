c19b525f72391fd5f883da4ba028f38386273a49
Dan Hackney <dan@haxney.org>
2010-02-06 09:02:59 -0500

    Forgot to add "anything" to dependencies.
    You would think that "anything" would be a logical dependency for
    "anything-contrib", wouldn't you?
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- anything-contrib-pkg.el
+++ anything-contrib-pkg.el
@@ -1,5 +1,6 @@
 (define-package "anything-contrib"
-  "2010.2.7.2" "Contributed Emacs Anything files"
-  '((auto-install "1.24")
+  "2010.2.7.3" "Contributed Emacs Anything files"
+  '((anything "1.241")
+    (auto-install "1.24")
     (emms "3.0")
     (slime "20091016")))
