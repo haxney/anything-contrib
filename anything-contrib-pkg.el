35658429c2314b9ec1c58aa53b1319053d9148de
Dan Hackney <dan@haxney.org>
2010-02-06 06:03:38 -0500

    Version bump for 2010.2.7.1
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- anything-contrib-pkg.el
+++ anything-contrib-pkg.el
@@ -1,5 +1,5 @@
 (define-package "anything-contrib"
-  "2010.2.7" "Contributed Emacs Anything files"
+  "2010.2.7.1" "Contributed Emacs Anything files"
   '((auto-install "1.24")
     (emms "3.0")
     (slime "20091016")))
